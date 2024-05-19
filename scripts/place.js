62a136b9076690e0ae77fecb47a204ced6e4d599
douglasrsantoss <santosdouglasroberto@gmail.com>
2024-05-18 20:34:19 -0400

    Week03 Assingment

---
+++ scripts/place.js
@@ -0,0 +1,9 @@
+
+function calculateWindChill(temperature,wind_speed){
+    let x = "something";
+    return x
+}
+
+// let wind_chill = document.querySelector("#wind_chill").textContent
+
+document.querySelector("#wind_chill").textContent = calculateWindChill(10, 10)
